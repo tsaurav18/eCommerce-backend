3dd635b789ecc20ba45d91cac1ce11b5a0f57593
tsaurav18 <tsaurav1711@gmail.com>
2025-05-21 10:37:16 +0900

    fix api url

--- ecombackend/settings.py
+++ ecombackend/settings.py
@@ -167,8 +167,8 @@ USE_TZ = False
 MEDIA_URL = "/media/"  # Use `/media/` instead of `/products/`
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
